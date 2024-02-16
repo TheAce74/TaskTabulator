bd989062fb47b3f59625aa4c146c859893428f43
The Ace <udonsichisom02@gmail.com>
2024-02-16 22:28:11 +0100

    chore: added an alert response for previous task addition

--- src/components/PreviousTasksButton.tsx
+++ src/components/PreviousTasksButton.tsx
@@ -1,8 +1,10 @@
 import { Button } from "@chakra-ui/react";
 import { useRef, useState } from "react";
-import { ChakraModal, Durations } from "../utils/types";
+import { ChakraAlert, ChakraModal, Durations } from "../utils/types";
 import { useChakraModal } from "../hooks/useChakraModal";
 import { format } from "date-fns";
+import { useChakraAlert } from "../hooks/useChakraAlert";
+import { timeout } from "../utils/timeout";
 
 type PreviousTasksButtonProps = {
   setDurationsList: (newDurationsList: Durations, day?: string) => void;
@@ -12,11 +14,17 @@ export default function PreviousTasksButton({
   setDurationsList,
 }: PreviousTasksButtonProps) {
   const [openModal, setOpenModal] = useState(false);
+  const [openAlert, setOpenAlert] = useState(false);
   const chakraModalRef = useRef<ChakraModal>({
     title: "",
     type: "table",
     buttonText: "",
     buttonCallback: () => "",
+  });
+  const chakraAlertRef = useRef<ChakraAlert>({
+    status: "success",
+    title: "",
+    message: "",
   });
 
   const handleOpenModal = () => {
@@ -27,6 +35,13 @@ export default function PreviousTasksButton({
       buttonCallback: (newDurationsList, today) => {
         if (today instanceof Date) {
           setDurationsList(newDurationsList, format(today, "PPPP"));
+          chakraAlertRef.current = {
+            status: "success",
+            title: "Success!",
+            message: "Task added successfully",
+          };
+          setOpenAlert(true);
+          timeout(() => setOpenAlert(false));
           setOpenModal(false);
           return true;
         }
@@ -42,12 +57,19 @@ export default function PreviousTasksButton({
     chakraModalRef.current
   );
 
+  const { alert } = useChakraAlert(
+    openAlert,
+    setOpenAlert,
+    chakraAlertRef.current
+  );
+
   return (
     <>
       <Button mb={8} onClick={handleOpenModal}>
         Add Previous Task
       </Button>
       {modal()}
+      {alert()}
     </>
   );
 }
