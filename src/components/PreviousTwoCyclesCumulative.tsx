c4f95ea2cbb7ad65e6014b1e0bd28e65526136a4
The Ace <udonsichisom02@gmail.com>
2024-04-03 22:57:12 +0100

    bugfix: fixed logic error in cumulative calculation

--- src/components/PreviousTwoCyclesCumulative.tsx
+++ src/components/PreviousTwoCyclesCumulative.tsx
@@ -1,7 +1,7 @@
 import { Heading } from "@chakra-ui/react";
 import { Tasks } from "../utils/types";
 import { getKeys } from "../utils/getKeys";
-import { compareDesc, format, parse } from "date-fns";
+import { compareDesc, eachDayOfInterval, format, parse } from "date-fns";
 import { getCustomToday } from "../utils/getCustomToday";
 import { getCumulative } from "../utils/getCumulative";
 
@@ -20,24 +20,20 @@ export default function PreviousTwoCyclesCumulative({
     const currentMonth = today.getMonth();
     const previousMonth = currentMonth === 0 ? 11 : currentMonth - 1;
     const previousTwoMonths = previousMonth === 0 ? 11 : previousMonth - 1;
+    const previousThreeMonths =
+      previousTwoMonths === 0 ? 11 : previousTwoMonths - 1;
     const currentYear = today.getFullYear();
-    const previousYear = currentYear - 1;
-    const previousCycleEndDate = new Date(2024, previousMonth, 19);
-    const previousCycleStartDate = new Date(2024, previousTwoMonths, 20);
-    return (
-      (day.getMonth() === previousTwoMonths &&
-        day.getDate() >= 20 &&
-        previousCycleEndDate.getDate() < 20 &&
-        (day.getFullYear() === currentYear ||
-          day.getFullYear() === previousYear)) ||
-      (day.getMonth() === previousMonth &&
-        day.getDate() < 20 &&
-        previousCycleStartDate.getDate() >= 20 &&
-        day.getFullYear() === currentYear) ||
-      (day.getMonth() === previousTwoMonths &&
-        day.getDate() >= 20 &&
-        day.getFullYear() === currentYear)
-    );
+    const interval = eachDayOfInterval({
+      start:
+        today.getDate() < 19
+          ? new Date(currentYear, previousThreeMonths, 20)
+          : new Date(currentYear, previousTwoMonths, 20),
+      end:
+        today.getDate() < 19
+          ? new Date(currentYear, previousTwoMonths, 19)
+          : new Date(currentYear, previousMonth, 19),
+    }).map((item) => format(item, "PPPP"));
+    return interval.includes(format(day, "PPPP"));
   });
   const lastTwoCyclesTasks = lastTwoCyclesDays.reduce(
     (acc, val) =>
